fd833b55811a8ea803369eb36540bc41045f33e8
Rostislav Kirillov <rast@tochka.com>
2026-02-04 02:18:18 +0400

    feat: trying gh actions

--- publish.sh
+++ publish.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -ex
 
+# get tag version "vX" from github or use placeholder
+raw_version="${GITHUB_REF_NAME:-v1}"
+version="${raw_version//v}"
+echo "$raw_version -> $version"
+if !  [ "${raw_version}" == v"${version}" ] ; then
+    echo "Tag is not in format 'vX': ${raw_version}"
+    exit 2
+fi
+if !  [ "$version" -eq "$version" ] 2>/dev/null; then
+    echo "Tag is not in format 'vX': ${version}"
+    exit 3
+fi
+
+echo "Version is ${version}"
+
 # building eboot.bin requires ps3toolchain
 # cd homebrew; make npdrm
 
