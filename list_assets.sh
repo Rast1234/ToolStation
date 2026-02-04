f700d348646b2ed35cacc8b8c2ed1adc157540a7
Rostislav Kirillov <rast@tochka.com>
2026-02-04 05:02:06 +0400

    fix: ci

--- list_assets.sh
+++ list_assets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-# script for github actions to list published files as release assets with readable labels
-echo -n '['
+# script for github actions to prepare published files as release assets
+echo -n '[' >> assets.json
 for p in _publish/* ;
 do
     platform=${p##*/}
@@ -10,8 +10,9 @@ do
         name=${file##*/}
         archive="${name} ${platform} build.zip"
         zip -j "${archive}" "${path}"
-        line='{"path":"'"${archive}"'","name":"'"${archive}"'","label":"'"${archive}"'"},'
-        echo -n "${line}"
+        #line='{"path":"'"${archive}"'","name":"'"${archive}"'","label":"'"${archive}"'"},'
+        line='{"path":"'"${archive}"'"},'
+        echo -n "${line}" >> assets.json
     done
 done
-echo -n ']'
+echo -n ']' >> assets.json
