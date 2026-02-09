6dc1a687e0bd22d2077522c76e970132df192537
Rostislav Kirillov <rast@tochka.com>
2026-02-10 01:09:00 +0400

    fix

--- list_assets.sh
+++ list_assets.sh
@@ -6,7 +6,7 @@ for p in _artifacts/publish/*/release_* ;
 do
     # p = _artifacts/publish/PROJECT/release_PLATFORM
     archive=$(echo $p | sed -r 's|_artifacts/publish/(.+)/release_(.+)/?$|\1.\2.zip|')
-    zip -j "${archive}" "${p}"
+    zip -j "${archive}" "${p}/*"
     line='{"path":"'"${archive}"'"},'
     echo -n "${line}" >> assets.json
 done
